95356dfdc4aa6305035607e1762efebbaa39cbad
Akira <caio_vini2006@hotmail.com>
2019-10-01 23:39:06 -0300

    Refatorando e modificando Table view e Celula
    Foi implimentado um metodo bind para linkar a caracteristicas da celula da table view e agora a celula possui um variavel Atividade para armazenar o estado da celula, alem disso, foi adicionado acoes aos botoes da celula, sendo agora possivel alterar os componentes da tela principal.

--- ProjetoIOS1/Cell/AtividadeTableViewCell.swift
+++ ProjetoIOS1/Cell/AtividadeTableViewCell.swift
@@ -8,26 +8,54 @@
 
 import UIKit
 
+protocol AtividadeTableViewCellDelegate {
+    func concluirAtividade(atividade: Atividade)
+}
+
 class AtividadeTableViewCell: UITableViewCell {
-
-       @IBOutlet weak var backgroundContentView: UIView!
-        
-        @IBOutlet weak var atividadeLabel: UILabel!
-        @IBOutlet weak var prioridadeSegmentedControl: UISegmentedControl!
-        @IBOutlet weak var concluidoSwitch: UISwitch!
-        
-        var atividade: Atividade?
-        
-        override func awakeFromNib() {
-            super.awakeFromNib()
-            
-    }
-
     
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-    }
-  
+    @IBOutlet weak var backgroundContentView: UIView!
     
+    @IBOutlet weak var atividadeLabel: UILabel!
+    @IBOutlet weak var prioridadeSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var concluidoSwitch: UISwitch!
+    
+    var atividade: Atividade!
+    
+    var delegate: AtividadeTableViewCellDelegate!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func bind(atividade: Atividade){
+        self.atividade = atividade
+        
+        self.atividadeLabel?.text  = self.atividade.nome
+        self.prioridadeSegmentedControl?.selectedSegmentIndex = self.atividade.prioridade.rawValue
+        self.concluidoSwitch?.isOn = self.atividade.concluida
+        
+        self.changeColor()
+    }
+    
+    func changeColor(){
+        switch self.atividade.prioridade {
+        case .BAIXA:
+            self.backgroundContentView?.backgroundColor = UIColor.green
+        case .MEDIA:
+            self.backgroundContentView?.backgroundColor = UIColor.orange
+        case .ALTA:
+            self.backgroundContentView?.backgroundColor = UIColor.red
+        }
+    }
+    
+    @IBAction func mudarPrioridade(_ sender: Any) {
+        self.atividade.prioridade = PrioridadeEnum(rawValue:self.prioridadeSegmentedControl.selectedSegmentIndex)!
+        self.changeColor()
+    }
+    
+    @IBAction func conclusaoAtividade(_ sender: Any) {
+        self.atividade.concluida = self.concluidoSwitch.isOn
+        self.delegate.concluirAtividade(atividade: self.atividade)
+    }
 }
--- ProjetoIOS1/Controller/MainViewController.swift
+++ ProjetoIOS1/Controller/MainViewController.swift
@@ -8,136 +8,122 @@
 
 import UIKit
 
-    class MainViewController: UIViewController{
-
-        var atividades: [[Atividade]] = []
+class MainViewController: UIViewController{
+    
+    var atividades: [[Atividade]] = []
+    
+    @IBOutlet var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        @IBOutlet var tableView: UITableView!
+        self.setupAtviidades()
         
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            
-            self.setupAtviidades()
-            
-            self.setupTableView()
+        self.setupTableView()
+    }
+    
+    func setupAtviidades() {
+        atividades.append([])
+        atividades.append([])
+        
+        atividades[0].append(Atividade(nome: "Beber Agua", prioridade: .ALTA))
+        atividades[0].append(Atividade(nome: "Tomar Whey", prioridade: .MEDIA))
+        
+        atividades[1].append(Atividade(nome: "Dormir", prioridade: .BAIXA, concluida: true))
+    }
+    
+    func setupTableView() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.register(UINib(nibName: "AtividadeTableViewCell", bundle: nil), forCellReuseIdentifier: "AtividadeTableViewCell")
+        
+    }
+    
+    func changeSection(atividade: Atividade, posSection: Int, posRow: Int){
+        if atividade.concluida && posSection == 0 {
+            self.atividades[0].remove(at: posRow)
+            self.atividades[1].append(atividade)
+        }else if !atividade.concluida && posSection == 1{
+            self.atividades[1].remove(at: posRow)
+            self.atividades[0].append(atividade)
         }
-
-        func setupAtviidades() {
-            atividades.append([])
-            atividades.append([])
+    }
+    
+    @IBAction func unwindSalvar(segue:UIStoryboardSegue) {
+        let svc = segue.source as! CriandoAtividadeViewController
+        
+        if let indexPath = self.tableView.indexPathForSelectedRow{
             
-            atividades[0].append(Atividade(nome: "Beber Agua", prioridade: .ALTA))
-            atividades[0].append(Atividade(nome: "Tomar Whey", prioridade: .MEDIA))
+            self.changeSection(atividade: self.atividades[indexPath.section][indexPath.row], posSection: indexPath.section, posRow: indexPath.row)
             
-            atividades[1].append(Atividade(nome: "Dormir", prioridade: .BAIXA, concluida: true))
+        }else{
+            self.atividades[0].append(svc.atividade!)
         }
-        
-        func setupTableView() {
-            self.tableView.delegate = self
-            self.tableView.dataSource = self
-            self.tableView.register(UINib(nibName: "AtividadeTableViewCell", bundle: nil), forCellReuseIdentifier: "AtividadeTableViewCell")
-            
-        }
-        
-      
-        @IBAction func unwindSalvar(segue:UIStoryboardSegue) {
-            let svc = segue.source as! CriandoAtividadeViewController
-                    
-            if let _	 = self.tableView.indexPathForSelectedRow{
-                self.tableView.reloadData()
-            }else{
-                self.atividades[0].append(svc.atividade!)
-                self.tableView.reloadData()
-            }
-         }
-        
-        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-            if(segue.identifier == "exibirAtividadeDetalhe"){
-                if let indexPath = self.tableView.indexPathForSelectedRow{
-                    let dvc = segue.destination as! CriandoAtividadeViewController
-                    
-                    dvc.atividade = atividades[indexPath.section][indexPath.row]
-                }
+        self.tableView.reloadData()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if(segue.identifier == "exibirAtividadeDetalhe"){
+            if let indexPath = self.tableView.indexPathForSelectedRow{
+                let dvc = segue.destination as! CriandoAtividadeViewController
+                
+                dvc.atividade = atividades[indexPath.section][indexPath.row]
             }
         }
     }
-            
-    extension MainViewController: UITableViewDelegate, UITableViewDataSource {
-        
-        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-           
-            performSegue(withIdentifier: "exibirAtividadeDetalhe", sender: self)
-        }
-        
-        func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-            if editingStyle == .delete {
-                self.atividades[indexPath.section].remove(at: indexPath.row)
-                tableView.deleteRows(at: [indexPath], with: .fade)
-            }
-        }
-        
-        func numberOfSections(in tableView: UITableView) -> Int {
-            return 2
-        }
-        
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return self.atividades[section].count
-        }
-        
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+}
 
-            let cell = tableView.dequeueReusableCell(withIdentifier: "AtividadeTableViewCell", for: indexPath) as! AtividadeTableViewCell
-           
-            
-            if indexPath.row < atividades[indexPath.section].count {
-                
-                let atividade = atividades[indexPath.section][indexPath.row]
-                
-                if(indexPath.section == 0  && !atividade.concluida){
-                    cell.atividadeLabel?.text  = atividade.nome
-                    cell.prioridadeSegmentedControl?.selectedSegmentIndex = atividade.prioridade.rawValue
-                    cell.concluidoSwitch?.isOn = atividade.concluida
-
-                    switch atividade.prioridade {
-                    case .BAIXA:
-                        cell.backgroundContentView?.backgroundColor = UIColor.green
-                    case .MEDIA:
-                        cell.backgroundContentView?.backgroundColor = UIColor.orange
-                    case .ALTA:
-                        cell.backgroundContentView?.backgroundColor = UIColor.red
-                    }
-                }else{
-                    cell.atividadeLabel?.text  = atividade.nome
-                    cell.prioridadeSegmentedControl?.selectedSegmentIndex = atividade.prioridade.rawValue
-                    cell.concluidoSwitch?.isOn = atividade.concluida
-
-                    switch atividade.prioridade {
-                    case .BAIXA:
-                        cell.backgroundContentView?.backgroundColor = UIColor.green
-                    case .MEDIA:
-                        cell.backgroundContentView?.backgroundColor = UIColor.orange
-                    case .ALTA:
-                        cell.backgroundContentView?.backgroundColor = UIColor.red
-                    }
-                }
-            }
-            
-            return cell
-            
-        }
-        
-        func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-            switch section {
-            case 0:
-                return "Atividades Abertas"
-            case 1:
-                return "Atividades Concluidas"
-            default:
-                return ""
-            }
-        }
-
-        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-            return UITableView.automaticDimension
+extension MainViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "exibirAtividadeDetalhe", sender: self)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            self.atividades[indexPath.section].remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.atividades[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AtividadeTableViewCell", for: indexPath) as! AtividadeTableViewCell
+        
+        let atividade = atividades[indexPath.section][indexPath.row]
+        
+        if(indexPath.section == 0  && !atividade.concluida){
+            cell.bind(atividade: atividade)
+        }else{
+            cell.bind(atividade: atividade)
+        }
+        
+        cell.delegate = self
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Atividades Abertas"
+        case 1:
+            return "Atividades Concluidas"
+        default:
+            return ""
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+}
+
